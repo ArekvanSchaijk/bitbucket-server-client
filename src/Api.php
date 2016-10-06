079cfab4b8d5eec6c186ef66d5e0b1638dd29b32
Arek van Schaijk <info@ucreation.nl>
2016-10-06 15:31:52 +0200

    Added API class

---
+++ src/Api.php
@@ -0,0 +1,12 @@
+<?php
+namespace ArekvanSchaijk\BitbucketServerClient;
+
+/**
+ * Class Api
+ * @package ArekvanSchaijk\BitbucketServerClient
+ * @author Arek van Schaijk <info@ucreation.nl>
+ */
+class Api
+{
+
+}
